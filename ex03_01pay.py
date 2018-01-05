553801e033f998d023f4a9a5495d2df45ebca329
Tracie Tung <tracietung@Tracies-MacBook-Pro.local>
2018-01-04 23:11:11 -0800

    ch3 feedback update

--- ex03_01pay.py
+++ ex03_01pay.py
@@ -2,12 +2,13 @@
 
 hours = float(input('Enter Hours: '))
 rate = float(input('Enter Rate: '))
-if hours > 40:
-    pay1 = 40 * rate
-    pay2 = (hours - 40) * (1.5 * rate)
+maxi_hours = 40
+if hours > maxi_hours:
+    pay1 = maxi_hours * rate    # pay of 40 hours
+    pay2 = (hours - maxi_hours) * (1.5 * rate) # pay of hours worked above 40 hours
     pay = pay1 + pay2
     print('Pay:', pay)
 
-elif hours <= 40:
-    pay3 = hours * rate
+elif hours <= maxi_hours:
+    pay3 = hours * rate    #pay of hours less than and equal to 40 hours
     print('Pay:', pay3)
