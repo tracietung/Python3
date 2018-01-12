43330c8cc62e99ee9eb93ce776b8c8e4da586494
Tracie Tung <tracietung@Tracies-MacBook-Pro.local>
2018-01-12 15:40:18 -0800

    ch5 extra-modified

--- ex0502max.py
+++ ex0502max.py
@@ -1,6 +1,6 @@
-#ch5 exercise2
-#Write another program that prompts for a list of numbers as above and at the
-#end prints out both the maximum and minimum of the numbers instead of the average.
+# ch5 exercise2
+# Write another program that prompts for a list of numbers as above and at the
+# end prints out both the maximum and minimum of the numbers instead of the average.
 
 print('Provide a list a number here; start to enter numbers')
 print('Enter "done" when you entered all the numbers.')
@@ -21,17 +21,15 @@ while True:
 print('You have entered {} numbers; they are {}' .format(count, mylist))
 
 x_largest = None
+x_smallest = None
 for itervar in mylist:
     if x_largest is None or itervar > x_largest :
         x_largest = itervar
-
-x_smallest = None
-for itervar in mylist:
-    if x_smallest is None or itervar < x_smallest:
+    elif x_smallest is None or itervar < x_smallest:
         x_smallest = itervar
 
 print('Largest:', x_largest, 'Smallest:', x_smallest)
 
-#largest = max(mylist)
-#smallest = min(mylist)
-#print('Max:', largest, 'Min:', smallest)
+# largest = max(mylist)
+# smallest = min(mylist)
+# print('Max:', largest, 'Min:', smallest)
