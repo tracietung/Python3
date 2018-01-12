e8caa540c6a0649961b3d2e607f053be09b5a5fc
Tracie Tung <tracietung@Tracies-MacBook-Pro.local>
2018-01-12 15:42:30 -0800

    vending machine

--- vending_loop_v2.py
+++ vending_loop_v2.py
@@ -1,6 +1,18 @@
 # ch5 vending machine - loop
 
-print('Healthy candy bar: $1.75 each.')
+print('Enter "a" for Healthy candy bar: $1.75 each')
+print('Enter "b" for Decaf Coke: $1.50 each')
+print('Enter "c" for Squid jerky: $2.25 each')
+
+# select the product
+while True:
+    selection = input('Select the product. Enter "a", "b", or "c": ')
+    if selection == 'a' or selection == 'b' or selection == 'c':
+        break
+    else:
+        print('Enter "a", "b", or "c" only')
+        continue
+# decide the amount
 while True:
     try:
         count = int(input('How many would you like to purchase? '))
@@ -12,24 +24,19 @@ while True:
         total = count * 1.75
         break
 
+# Insert money
 print('Your total is', total)
 print('''Enter ".05" for a nickel;
 ".10" for a dime;
 ".25" for a quarter;
  or "1" for 1.00 dollar bill''')
 
-
 sumup = 0
 while True:
-#    inp_money = float(input('Insert a value per time: '))
-    for value in [.05, .10, .25, 1]:
+    while True:
         inp_money = input('Insert a value per time: ')
-        try:
-            fmoney = float(inp_money)
-        except:
-            print('Enter ".05", ".10", ".25" or "1" only')
-            continue
-        if fmoney == .05 or fmoney ==.10 or fmoney ==.25 or fmoney == 1:
+        fmoney = float(inp_money)
+        if fmoney == .05 or fmoney == .10 or fmoney == .25 or fmoney == 1:
             break
         else:
             print('Enter ".05", ".10", ".25" or "1" only')
@@ -39,6 +46,7 @@ while True:
     if sumup >= total:
         break
 
+# give changes
 changes = sumup - total
 
 quarter = changes // .25
